279fcb2dd89840ec27db4487fa2f6d5190826ff8
e4rache <e4rache@protonmail.com>
2018-08-08 17:28:07 +0200

    CORS

--- app.js
+++ app.js
@@ -21,23 +21,8 @@ mongoose.connect(
   connectionString,
   { useNewUrlParser: true }
 );
+
 // CORS
-/*
-app.use((req, res, next) => {
-  res.header("Acess-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Request-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    // It doesn't need to continue to the routes
-    // if the method was OPTIONS
-    return res.status(200).json({});
-  }
-  next();
-});
-*/
 
 app.use(cors());
 
